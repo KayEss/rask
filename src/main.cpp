499162e7b2c52898cbc0fd209a49be934d5ed3c1
Kirit Sælensminde <kirit@felspar.com>
2026-06-24 14:10:07 +0700

    Start a real main: raise FD limit and log startup
    Call promise_to_never_use_select first thing to lift the open file
    descriptor limit, then log at info level that rask has started along
    with the old and new limits.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include <felspar/io/posix.hpp>
+#include <planet/log.hpp>
 
 
 int main() {
-    std::cout << "rask\n";
+    auto const [old_limit, new_limit] =
+            felspar::posix::promise_to_never_use_select();
+    planet::log::info(
+            "rask started -- raised open file descriptor limit from", old_limit,
+            "to", new_limit);
     return 0;
 }
